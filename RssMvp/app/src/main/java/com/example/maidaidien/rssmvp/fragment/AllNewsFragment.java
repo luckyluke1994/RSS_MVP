5686f8cd817ca581c4fedf2a57e81127f7acf84d
luckyluke1994 <mddien1994@gmail.com>
2017-03-24 10:54:41 +0700

    add tabs

--- RssMvp/app/src/main/java/com/example/maidaidien/rssmvp/fragment/AllNewsFragment.java
+++ RssMvp/app/src/main/java/com/example/maidaidien/rssmvp/fragment/AllNewsFragment.java
@@ -14,6 +14,7 @@ import com.example.maidaidien.rssmvp.R;
  */
 
 public class AllNewsFragment extends Fragment {
+    public static final String TITLE = "All News";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
