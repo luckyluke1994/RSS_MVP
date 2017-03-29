5f70629f612fca5df53050632f6179d036d3066b
luckyluke1994 <mddien1994@gmail.com>
2017-03-29 14:50:57 +0700

    create service

---
+++ RssMvp/app/src/main/java/com/example/maidaidien/rssmvp/presenter/Callbacks.java
@@ -0,0 +1,13 @@
+package com.example.maidaidien.rssmvp.presenter;
+
+import android.content.Context;
+
+/**
+ * Created by mai.dai.dien on 29/03/2017.
+ */
+
+public class Callbacks {
+    public interface View {
+        public Context getContext();
+    }
+}
